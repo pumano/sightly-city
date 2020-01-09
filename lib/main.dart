9d5bb1e519595f7effd7c3b25047a4056b375a6b
Dmitry Zakharov <pumano@gmail.com>
2020-01-09 09:17:38 +0300

    feat(theme): add support of dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,9 @@ void main() => runApp(MyApp());
 // #AC956B; // sand background and sand text too
 // #f1e4cb // sub sand
 
-// #393E6E; // navy link 
-
+// #393E6E; // navy link
 
 // #5D5D5D // gray color text
-
-const PrimaryColor = const Color(0xFFAB9677);
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -35,8 +32,34 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primaryColor: PrimaryColor,
+        primaryColor: Color(0xFFAB9677),
         primarySwatch: Colors.blue,
+        primaryColorBrightness: Brightness.light,
+
+        textTheme: TextTheme(
+          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          title: TextStyle(fontSize: 36.0, color: Color(0xFFAC956B), fontFamily: "PoiretOne"),
+          subtitle: TextStyle(fontSize: 26.0, fontFamily: "HelveticaNeueCyrThin"),
+          body1: TextStyle(
+                            fontFamily: "HelveticaNeueCyrThin",
+                            fontSize: 18.0, color: Color(0xFF5D5D5D)),
+          body2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          subhead: TextStyle(fontFamily: "HelveticaNeueCyrThin", fontSize: 20),
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        textTheme: TextTheme(
+          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          title: TextStyle(fontSize: 36.0, color: Color(0xFFAC956B), fontFamily: "PoiretOne"),
+          subtitle: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, fontFamily: "HelveticaNeueCyrThin"),
+          body1: TextStyle(
+                            letterSpacing: 1.15,
+                            fontFamily: "HelveticaNeueCyrThin",
+                            fontSize: 18.0),
+          body2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          subhead: TextStyle(fontFamily: "PoiretOne", fontSize: 20),
+        )
       ),
       routes: Routes().routes,
       initialRoute: '/splash',
