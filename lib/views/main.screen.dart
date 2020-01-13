909c85e575add06c0d5d4f87cebdac8868b272b0
Dmitry Zakharov <pumano@gmail.com>
2020-01-13 12:03:22 +0300

    fix(main): fix gallery width

--- lib/views/main.screen.dart
+++ lib/views/main.screen.dart
@@ -22,38 +22,48 @@ class _MainScreenState extends State<MainScreen> {
         appBar: AppBar(
             title: Text('Видный Город',
                 style: Theme.of(context).textTheme.subhead)),
-        body: SafeArea(
-            child: Container(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    CarouselSlider(
-                      height: 200.0,
-                      items: imgList.map((i) {
-                        return Builder(
-                          builder: (BuildContext context) {
-                            return Container(
-                                width: MediaQuery.of(context).size.width,
-                                margin: EdgeInsets.symmetric(horizontal: 5.0),
-                                decoration:
-                                    BoxDecoration(color: Colors.black87),
-                                child: Image(
-                                  image: AssetImage('assets/${i}'),
-                                ));
-                          },
-                        );
-                      }).toList(),
-                    ),
-                    Container(
-                        padding: EdgeInsets.only(top: 10, bottom: 20),
-                        child: Text("Светский пригород",
-                            style: Theme.of(context).textTheme.title)),
-                    Text(
-                        "«Видный Город» расположен всего в 6 км от Москвы по Варшавскому шоссе, в зеленом уголке Подмосковья, богатом историческими усадебными традициями и природными достопримечательностями. До ближайшего метро - всего 10-15 минут на собственном или общественном транспорте.",
-                        style: Theme.of(context).textTheme.body1)
-                  ],
-                ))),
+        body: Container(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+              CarouselSlider(
+                height: 200.0,
+                enlargeCenterPage: true,
+                items: imgList.map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(
+                              color:
+                                  MediaQuery.of(context).platformBrightness ==
+                                          Brightness.dark
+                                      ? Colors.black87
+                                      : Colors.transparent),
+                          child: Image(
+                            image: AssetImage('assets/${i}'),
+                          ));
+                    },
+                  );
+                }).toList(),
+              ),
+              Container(
+                  padding:
+                      EdgeInsets.only(bottom: 20.0, left: 20.0, right: 20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                          padding: EdgeInsets.only(top: 10, bottom: 20),
+                          child: Text("Светский пригород",
+                              style: Theme.of(context).textTheme.title)),
+                      Text(
+                          "«Видный Город» расположен всего в 6 км от Москвы по Варшавскому шоссе, в зеленом уголке Подмосковья, богатом историческими усадебными традициями и природными достопримечательностями. До ближайшего метро - всего 10-15 минут на собственном или общественном транспорте.",
+                          style: Theme.of(context).textTheme.body1)
+                    ],
+                  ))
+            ])),
         drawer: NavigationDrawer());
   }
 }
